e288a285653c3ba8c9608a77ee6d67fcf4acbecc
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2018-08-18 18:31:15 +0000

    Added envirnment variables

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index")
 
-// mongoose.connect('mongodb://localhost:27017/yelp_camp_4',{ useNewUrlParser: true });
-mongoose.connect("mongodb://hardik:2Vv!1nQWPsO^@ds125872.mlab.com:25872/yelpcamp",{ useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL,{ useNewUrlParser: true });
+// mongoose.connect("mongodb://hardik:2Vv!1nQWPsO^@ds125872.mlab.com:25872/yelpcamp",{ useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
